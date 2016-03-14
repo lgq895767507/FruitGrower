161c917e20483b167eeb733f7d0c56d82fed3024
gqliu1 <gqliu1@lewatek.com>
2016-03-14 21:24:23 +0800

    add mainfragment.xml

---
+++ app/src/main/java/com/lgq/fruitgrower/view/adapter/StatusAdapter.java
@@ -0,0 +1,40 @@
+package com.lgq.fruitgrower.view.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.lgq.fruitgrower.model.beans.Consumer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lgq on 16-3-14.
+ */
+public class StatusAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Consumer> datas;
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
